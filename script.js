4d45acacd050aafe71723f1732c85785a54b11ee
Garrett <garrettgassensmith@gmail.com>
2020-02-27 13:19:29 -0600

    Added some very basic styling to the page with TailwindCSS and got the timer with button working

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
-let totalTime = 5 * 60 * 1000;
-let penaltyTime = 30 * 1000;
-let tickTime = 1000;
 
-function formatTime(ms) {
-  let minutes = Math.floor(ms / 60000);
-  let seconds = ms % 60000;
-  seconds /= 1000;
-  if (seconds < 10) {
-    seconds = "0" + seconds;
+// Timer
+const startBtn = document.getElementById('start')
+let totalTime = 5 * 60 * 1000
+// let penaltyTime = 30 * 1000
+let tickTime = 1000
+
+function timer() {
+  function formatTime(ms) {
+    let minutes = Math.floor(ms / 60000)
+    let seconds = ms % 60000
+    seconds /= 1000
+    if (seconds < 10) {
+      seconds = "0" + seconds
+    }
+    let timerEl = document.getElementById('timer')
+    timerEl.innerHTML = `${minutes}:${seconds}`
   }
-  let timerEl = document.getElementById("timer");
-  timerEl.textContent = `${minutes}:${seconds}`;
+  function tick() {
+    totalTime -= tickTime
+    formatTime(totalTime)
+  }
+  setInterval(tick, tickTime)
 }
 
-function tick() {
-  totalTime -= tickTime;
-  formatTime(totalTime);
-}
-
-let myInterval = setInterval(tick, tickTime);
-
-let wrongEl = document.getElementById("wrong");
-
-wrongEl.addEventListener("click", function() {
-  totalTime -= penaltyTime;
-  formatTime(totalTime);
-});
+startBtn.addEventListener('click', timer)
