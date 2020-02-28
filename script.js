6c810305e97f45605879a4ba8f36511c6084d715
Garrett <garrettgassensmith@gmail.com>
2020-02-28 14:45:35 -0600

    Working on storing highscores locally and rendering scoreboard

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const timerLabel = document.getElementById('timerLabel')
 const introCard = document.getElementById('introCard')
 const outroCard = document.getElementById('outroCard')
 const score = document.getElementById('score')
-const user = document.getElementById('userInitials')
-const submitBtn = document.getElementById('submit')
+const userInput = document.getElementById('userInitials')
+const scoreForm = document.getElementById('scoreForm')
 const gameOver = document.getElementById('gameOver')
 const resetBtn1 = document.getElementById('tryAgain1')
 const viewHighscores = document.getElementById('viewHighscores')
@@ -111,9 +111,24 @@ let totalTime = 2 * 60 * 1000
 let penaltyTime = 15 * 1000
 let tickTime = 1000
 let finalScore = ''
-let highscores = localStorage.getItem("score")
+let highscores = []
+let selectScore = 
 
 // Questions
+function askQuestion() {
+  // Display question card
+  feedback.style.display = "none"
+  questionCard.style.display = "flex"
+  // Set question number and get random question from questions array
+  question = questions[Math.floor(Math.random() * questions.length)]
+  // Insert in HTML
+  questionNumber.innerHTML = `Question ${questionIndex}`
+  questionContent.innerHTML = `${question.content}`
+  answer1.innerHTML = `${question.answer1}`
+  answer2.innerHTML = `${question.answer2}`
+  answer3.innerHTML = `${question.answer3}`
+  answer4.innerHTML = `${question.answer4}`
+}
 function askQuestion() {
   // Display question card
   feedback.style.display = "none"
@@ -171,7 +186,7 @@ function timer() {
       clearInterval(interval)
       timerEl.style.color = "green"
       stopQuiz()
-    }
+    } 
   }
 }
 
@@ -192,7 +207,98 @@ function stopQuiz() {
   }
 }
 function resetQuiz() {
-
+  totalTime = 2 * 60 * 1000
+  questions = [
+    {
+      content: "This is question number 1, it'll have a really interesting question that you have to answer.",
+      answer1: "totally wrong",
+      answer2: "correct",
+      answer3: "nah",
+      answer4: "almost"
+    },
+    {
+      content: "This is question number 2, it'll be different than question 1 but you pretty much do the same thing and answer it to the best of your ability.",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 3",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 4",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 5",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 6",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 7",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 8",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 9",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+    {
+      content: "This is question 10",
+      answer1: "good try",
+      answer2: "you suck",
+      answer3: "not even close",
+      answer4: "correct"
+      
+    },
+  ]
+  question = ''
+  questionIndex = 1
+  scoreboardCard.style.display = "none"
+  introCard.style.display = "none"
+  outroCard.style.display = "none"
+  gameOver.style.display = "none"
+  questionCard.style.display = "flex"
+  timerEl.style.color = "#805ad5"
+  timer()
+  askQuestion()
 }
 
 // Scores
@@ -202,16 +308,62 @@ function calcScore() {
   let finalSecond = finalTime.charAt(2) + finalTime.charAt(3)
   finalScore = (finalMinute * 60) + parseInt(finalSecond)
 }
-function submitScore(){
+function submitScore(event) {
+  event.preventDefault();
+  init()
   outroCard.style.display = "none"
   scoreboardCard.style.display = "flex"
-  let addUserScore = finalScore + ` - ${user.value}`
+  let addUserScore = finalScore + ` - ${userInput.value}`
   let newScore = document.createElement("li")
   newScore.textContent = addUserScore
   scoreboard.appendChild(newScore)
+  let userText = userInput.value.trim()
+  // highscores.push(userText)
+  // userInput.value = ""
+  // storeHighscores();
+  // renderHighscores();
+}
+function deleteScore(event){
+    let element = event.target
+    console.log(element)
+    // Get its data-index value and remove the highscore from the scoreboard
+    let index = element.getAttribute("data-index")
+    highscores.splice(index, 1)
+    storeHighscores()
+    renderHighscores()
 }
 function showHighscores() {
   scoreboardCard.style.display = "flex"
+  introCard.style.display = "none"
+  outroCard.style.display = "none"
+  questionCard.style.display = "none"
+}
+function renderHighscores() {
+  // Clear scoreboard
+  scoreboard.innerHTML = "";
+  // Render a new li for each highscore
+  for (var i = 0; i < scoreboard.length; i++) {
+    let highscore = highscores[i]
+    let li = document.createElement("li")
+    li.textContent = highscore
+    li.setAttribute("data-index", i)
+    scoreboard.appendChild(li);
+  }
+}
+function init() {
+  // Get stored highscores from localStorage
+  // Parsing the JSON string to an object
+  var storedHighscores = JSON.parse(localStorage.getItem("highscores"));
+  // If highscores were retrieved from localStorage, update the highscores array to it
+  if (storedHighscores !== null) {
+    highscores = storedHighscores
+  }
+  // Render highscores to the DOM
+  renderHighscores()
+}
+function storeHighscores() {
+  // Stringify and set highscores key in localStorage to highscores array
+  localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
 // Buttons
@@ -220,9 +372,9 @@ answer1.addEventListener('click', answerQuestion)
 answer2.addEventListener('click', answerQuestion)
 answer3.addEventListener('click', answerQuestion)
 answer4.addEventListener('click', answerQuestion)
-submitBtn.addEventListener('click', submitScore)
-// resetBtn1.addEventListener('click', resetQuiz)
+scoreForm.addEventListener("submit", submitScore)
+resetBtn1.addEventListener('click', resetQuiz)
+clearScore.addEventListener('click', deleteScore)
 viewHighscores.addEventListener('click', showHighscores)
-// resetBtn2.addEventListener('click', resetQuiz)
-
+resetBtn2.addEventListener('click', resetQuiz)
 
